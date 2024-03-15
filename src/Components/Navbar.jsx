1bc45c5143a12dadb4128246464d9fe9ac53ea7c
Gaurav Chand <gauravchand1809@gmail.com>
2024-03-15 12:03:38 +0530

    sixe changed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <div className="flex justify-between items-center p-8 border">
         <div>
-            <h1 className="text-6xl ml-6">Arun</h1>
+            <h1 className="text-3xl ml-6">Arun</h1>
         </div>
         <div>
             <ul  className="flex gap-4 justify-center items-center">
