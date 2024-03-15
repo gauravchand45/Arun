cedf699a63eb106c2bc76e2f391f120d9b1e2447
Gaurav Chand <gauravchand1809@gmail.com>
2024-03-15 20:23:53 +0530

    responsive

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,16 +2,16 @@ import './utility.css'
 
 function Navbar() {
   return (
-    <div className="flex justify-between items-center p-10  mx-20 font-serif border-b-4 ">
-        <div>
-            <h1 className="text-7xl font-bold text-[#214559]  hover:scale-[1.04] text-left animate-slide-in-left ">Arun</h1>
+    <div className="md:flex justify-between items-center p-5 md:p-10  md:mx-20 font-serif border-b-4 ">
+        <div class="text-center md:text-left">
+            <h1 className="text-4xl md:text-7xl font-bold text-[#214559]  hover:scale-[1.04] animate-slide-in-left ">Arun</h1>
         </div>
-        <div className='text-right animate-slide-in-right'>
+        <div className='mt-5 md:mt-0 text-center md:text-right animate-slide-in-right'>
             <ul  className="flex gap-5 justify-center items-center text-xl text-gray-500">
-                <a href="/"><li className="cursor-pointer hover:underline hover:text-gray-700">Photography</li></a>
-                <a href="/"><li className="cursor-pointer hover:underline hover:text-gray-700">Books</li></a>
-                <a href="/"><li className="cursor-pointer hover:underline hover:text-gray-700">Texts</li></a>
-                <a href="/"><li className="cursor-pointer hover:underline hover:text-gray-700">Courses</li></a>
+                <a href=""><li className="cursor-pointer hover:underline hover:text-gray-700">Photography</li></a>
+                <a href=""><li className="cursor-pointer hover:underline hover:text-gray-700">Books</li></a>
+                <a href=""><li className="cursor-pointer hover:underline hover:text-gray-700">Texts</li></a>
+                <a href=""><li className="cursor-pointer hover:underline hover:text-gray-700">Courses</li></a>
             </ul>
         </div>
     </div>
