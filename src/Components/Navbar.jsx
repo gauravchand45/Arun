6d6794f4b9b89257f24309744659f42c7b587b49
Gaurav Chand <gauravchand1809@gmail.com>
2024-03-15 20:37:54 +0530

    responsive

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import './utility.css'
 function Navbar() {
   return (
     <div className="md:flex justify-between items-center p-5 md:p-10  md:mx-20 font-serif border-b-4 ">
-        <div class="text-center md:text-left">
+        <div className="text-center md:text-left">
             <h1 className="text-4xl md:text-7xl font-bold text-[#214559]  hover:scale-[1.04] animate-slide-in-left ">Arun</h1>
         </div>
         <div className='mt-5 md:mt-0 text-center md:text-right animate-slide-in-right'>
